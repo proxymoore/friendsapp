c624efda49e828e4052d2a23c56e91699fa4d36a
Darcie Moore <70707755+proxymoore@users.noreply.github.com>
2023-03-30 15:53:08 -0600

    Updating multipule files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # For when we push this out of the development stage, we would change the host to whatever site is hosting
+  #config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
